5ad95fdb3df86490e1010bbe7e2a1d4ec31717b8
Ironlaydee <daintiestdolly@gmail.com>
2023-08-01 09:37:19 +0000

    update items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,14 @@
-export default function updateUniqueItems(_map) {
-  if (!(_map instanceof Map)) throw Error('Cannot process');
-  _map.forEach((x, y) => {
-    if (x === 1) _map.set(y, 100);
-  });
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
 
-  return _map;
+    return map;
+  }
+
+  throw new Error('Cannot process');
 }
 
